763f32cf10d527ea94010d242b313b0f458022dd
barjoco <barjo.email@gmail.com>
2020-07-21 22:09:14 +0100

    updated

--- sxhkd/bookmarks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-res=$(find /mnt/Portable/Bookmarks/ -type f | sed s,/mnt/Portable/Bookmarks/,, | rofi -dmenu -i -p Bookmarks:)
-
-if [ "$res" != "" ]; then
-    firefox $(cat /mnt/Portable/Bookmarks/"$res")
-fi
