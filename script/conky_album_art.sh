6df89216a1954e7c7f1862633bdd11808490f5d8
Adam <adambartonjones@gmail.com>
2020-10-18 21:26:55 +0100

    updated

--- script/conky_album_art.sh
+++ script/conky_album_art.sh
@@ -7,20 +7,20 @@
 current_track_id=''
 
 while true; do
-    this_track_id="$(playerctl --player=spotify metadata mpris:trackid)"
 
-    if [ "$current_track_id" != "$this_track_id" ]; then
-        echo "Song has changed: $this_track_id"
-        imgid=$(playerctl --player=spotify metadata mpris:artUrl | grep -o [^/]*$)
-        echo "Image: $imgid"
-        wget -q "https://i.scdn.co/image/$imgid" -O ~/Pictures/album_art
-        current_track_id="$this_track_id"
-        killall conky
-        conky -d
-        echo 'Conky restarted'
-    else
-        echo "Song is the same: $this_track_id"
+    if [ "$(playerctl -l | grep spotify)" ]; then
+
+        this_track_id="$(playerctl --player=spotify metadata mpris:trackid)"
+
+        if [ "$current_track_id" != "$this_track_id" ]; then
+            imgid=$(playerctl --player=spotify metadata mpris:artUrl | grep -o [^/]*$)
+            wget -q "https://i.scdn.co/image/$imgid" -O ~/Pictures/album_art
+            current_track_id="$this_track_id"
+            killall conky
+            conky -d
+        fi
+
     fi
 
-	sleep 2
+    sleep 2
 done
